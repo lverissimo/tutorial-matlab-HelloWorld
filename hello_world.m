2fd1befacf2937b72f5e661af22876ae3f4a2092
lverissimo <net@luisverissimo.com>
2023-01-08 17:40:27 +0000

    novo commit

--- hello_world.m
+++ hello_world.m
@@ -1,6 +1,6 @@
 function hello_world
     fprintf('\n=============')
-    fprintf('\nAgora é que é!!! Olá\n')
+    fprintf('\nAgora é que é!!! Olá 2\n')
     fprintf('\nHello, World!\n')
     fprintf('=============\n')
 end
